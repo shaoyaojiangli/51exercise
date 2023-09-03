546aee1e5149aa80d89f9a3b9dd02e9e3c1a345f
shaoyaojiangli <1909914367@qq.com>
2023-09-03 20:11:22 +0800

    呼吸灯

---
+++ 15LED呼吸灯/main.c
@@ -0,0 +1,39 @@
+#include <REGX52.H>
+
+sbit LED = P2^0;
+
+void Delay(unsigned int t)
+{
+    while(t--);
+}
+
+int main()
+{
+    unsigned char Time,i;
+    while(1)
+    {
+        for(Time = 0; Time < 100; Time ++)
+        {
+            for(i = 0; i < 20; i++)
+            {
+                
+                LED = 0;
+                Delay(Time);
+                LED = 1;
+                Delay(100-Time);
+            }
+        }
+   
+         for(Time = 100; Time > 0; Time --)
+        {
+            for(i = 0; i < 20; i++)
+            {
+                
+                LED = 0;
+                Delay(Time);
+                LED = 1;
+                Delay(100-Time);
+            }
+        }
+    }
+}
